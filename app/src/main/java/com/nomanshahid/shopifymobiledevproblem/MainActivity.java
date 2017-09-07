dfddb454ae6c9e3345441a112d30ba6c22713e3c
Noman <noman-shahid@hotmail.com>
2017-09-07 09:39:46 -0400

    Testing customer total spent

--- app/src/main/java/com/nomanshahid/shopifymobiledevproblem/MainActivity.java
+++ app/src/main/java/com/nomanshahid/shopifymobiledevproblem/MainActivity.java
@@ -1,7 +1,16 @@
 package com.nomanshahid.shopifymobiledevproblem;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 
@@ -19,27 +28,70 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         String shopifyURL = "https://shopicruit.myshopify.com/admin/orders.json?page=1&" +
                 "access_token=c32313df0d0ef512ca64d5b336a0d7c6";
-        OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder()
-                .url(shopifyURL)
-                .build();
 
-        Call call = client.newCall(request);
-        call.enqueue(new Callback() {
-            @Override
-            public void onFailure(Call call, IOException e) {
+        if (isNetworkAvailable()) {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url(shopifyURL)
+                    .build();
 
-            }
-
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                if (response.isSuccessful()) {
-
+            Call call = client.newCall(request);
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    Toast.makeText(getApplicationContext(), "There was an error. Please try again.",
+                            Toast.LENGTH_LONG).show();
                 }
 
-            }
-        });
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    if (response.isSuccessful()) {
+                        String jsonData = response.body().string();
+                        try {
+                            JSONObject jsonObject = new JSONObject(jsonData);
+                            Log.v("MainActivity", String.valueOf(getCustomerInfo(jsonObject, "Napolean", "Batz")));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
+                    } else {
+                        Toast.makeText(getApplicationContext(), "There was an error. Please try again.",
+                                Toast.LENGTH_LONG).show();
+                    }
+
+                }
+            });
+        } else {
+            Toast.makeText(getApplicationContext(), "No network available! Please connect and " +
+                            "try again.",
+                    Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+
+    private double getCustomerInfo(JSONObject jsonObject, String firstName, String lastName) throws JSONException {
+        double totalSpent = 0;
+        JSONArray orders = jsonObject.getJSONArray("orders"); //check for CAD and total_spent
+        for (int i = 0; i < orders.length(); ++i) {
+            JSONObject order = orders.getJSONObject(i);
+            JSONObject customer = order.getJSONObject("customer");
+            if (customer.getString("first_name") == firstName &&
+                    customer.getString("last_name") == lastName) {
+                totalSpent += order.getDouble("total_price");
+            }
+        }
+        return totalSpent;
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if (networkInfo != null && networkInfo.isConnected()) {
+            isAvailable = true;
+        }
+        return isAvailable;
 
     }
 }
